a6b343772c259380b11da4937ffb1411c77fffaa
Saswat PC Ubuntu <saswatjungkhadka@gmail.com>
2026-02-26 10:42:06 +0000

    Arenas

---
+++ arena.c
@@ -0,0 +1,59 @@
+#include "arena.h"
+
+ARENA_ERROR makeArena(Arena* arena, u64 sizeInBytes){
+    void* tempAddress = malloc(sizeInBytes);
+    if (tempAddress == NULL)
+        return ARENA_CREATION_FAILURE;
+    // Batch write to the struct to avoid cache misses.
+	arena->arenaBasePointer = tempAddress;
+	arena->arenaSize = sizeInBytes;
+	arena->arenaSizeCommitted = sizeInBytes;
+	arena->arenaSizeLeftInCommitted = sizeInBytes;
+	arena->arenaLatestPointer = tempAddress;
+    return ARENA_OK;
+}
+
+ARENA_ERROR pushData(Arena* arena, void* data, u64 sizeInBytes, void** outMemoryPointer){
+    if (sizeInBytes > arena->arenaSizeLeftInCommitted)
+        return ARENA_NO_SPACE_FOR_DATA;
+    if (data != NULL)
+        memcpy(arena->arenaLatestPointer, data, sizeInBytes);
+    byte* newAddress = (byte*)(arena->arenaLatestPointer);
+    newAddress += sizeInBytes;
+    *outMemoryPointer = arena->arenaLatestPointer;
+    arena->arenaLatestPointer = (void*)(newAddress);
+    arena->arenaSizeLeftInCommitted = arena->arenaSizeLeftInCommitted - sizeInBytes;
+    return ARENA_OK;
+}
+
+ARENA_ERROR popData(Arena* arena, u64 sizeInBytes){ // Since we want to pop items as a stack, we won't be deleting this from the middle anyways.
+    if (sizeInBytes > arena->arenaSizeCommitted) {
+        return ARENA_CANT_DELETE_RESERVED;
+    }
+    if (sizeInBytes > (arena->arenaSizeCommitted - arena->arenaSizeLeftInCommitted))
+        return ARENA_CANT_DELETE_NULL;
+    byte* newArenaPointer = (byte*)(arena->arenaLatestPointer) - sizeInBytes;
+    arena->arenaSizeLeftInCommitted = arena->arenaSizeLeftInCommitted - sizeInBytes;
+    arena->arenaLatestPointer = (void*)(newArenaPointer);
+    return ARENA_OK;
+}
+
+ARENA_ERROR removeArena(Arena* arena){
+    free(arena->arenaBasePointer);
+    return ARENA_OK;
+}
+
+ARENA_ERROR castPointer(Arena* arena, void** outMemoryPointer){
+    *outMemoryPointer = arena->arenaLatestPointer;
+    return ARENA_OK;
+}
+
+ARENA_ERROR pushPointer(Arena* arena, u32 offsetInBytes){
+    if(offsetInBytes > arena->arenaSizeLeftInCommitted){
+        return ARENA_NO_SPACE_FOR_DATA;
+    }
+    byte* newArenaPointer = (byte*)(arena->arenaLatestPointer) + offsetInBytes;
+    arena->arenaLatestPointer = (void*)(newArenaPointer);
+    return ARENA_OK;
+}
+
